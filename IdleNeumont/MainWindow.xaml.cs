0b3798efdd1fd12ad4714bc60b63f8181ecab3b4
mmargolin <mmargolin@student.neumont.edu>
2021-11-18 16:44:58 -0700

    COMMENTING

--- IdleNeumont/MainWindow.xaml.cs
+++ IdleNeumont/MainWindow.xaml.cs
@@ -18,13 +18,10 @@ using System.IO;
 
 namespace IdleNeumont
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
    
     public partial class MainWindow : Window
     {
+        // Don't worry about this
         bool genStop = false;
 
         public MainWindow()
@@ -32,17 +29,17 @@ namespace IdleNeumont
             InitializeComponent();
 
             
-
+            // TIMER TimeSpan SYNTAX -> (Days, Hours, Minutes, Seconds, Milliseconds)
             timer.Interval = new TimeSpan(0,0,0,1,300);
+            // WHEN INTERVAL -> RUN loadStart
             timer.Tick += loadStart;
+
             timer.Start();
             startSound.Play();
 
 
-
+            // Thread creaton stuff
             ThreadStart backRef = new ThreadStart(callBackThread);
-          
-
             Thread backThread = new Thread(backRef);
             backThread.Start();
 
@@ -53,14 +50,18 @@ namespace IdleNeumont
 
         
 
-        // System.Windows.Media.MediaPlayer startSound = new System.Windows.Media.MediaPlayer();    
+        
+        // CREATE SOUND -> DIRECTORY MANAGEMENT FOR RELATIVE FILEPATH
         System.Media.SoundPlayer startSound = new System.Media.SoundPlayer(((Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).Parent.FullName) + @"\IdleNeumont\Resources\startup.wav"));
            
+        // CREATE TIMER
         System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
 
+
+        // SECOND THREAD FOR BACKGROUND
         public void callBackThread()
         {
-
+                // THIS IS EXAMPLE OF OWNERSHIP SHARE/ TRANSFER 
 /*             
                 Action action = () =>
                 {
@@ -72,23 +73,42 @@ namespace IdleNeumont
         }
 
 
+
+        // WEIRD TIMER SHIT
         private void loadStart(object sender, EventArgs e)
         {
+            // Stop timer To Run
             timer.Stop();
-            
+
+            // Load Main Start Page
             stackStart.Visibility = Visibility.Visible;
+
+            // Load Neumont Img
             neumontStart.Visibility = Visibility.Visible;
+            
+            // New Timer Shit
             timer.Interval = new TimeSpan(0, 0, 0, 2, 200);
             timer.Tick += chaosStart;
             timer.Start();
         }
 
+        // WEIRD TIMER SHIT 2 ELECTRIC BOOGALOO
         private void chaosStart(object sender, EventArgs e)
         {
+
+            // Stop thing to run thing bla bla bla
             timer.Stop();
+
+            // Load Text Img
             chaoticStart.Visibility = Visibility.Visible;
-           
+
+            // Another timer shit
+           // timer.Interval = new TimeSpan(0, 0, 0, 2, 0);
+           // timer.Tick += buttonStart;
+           // timer.Start();
         }
+
+
        
         
         
