39147e673f1c66f55ce3b8b3e971332a068a7db0
mmargolin <mmargolin@student.neumont.edu>
2021-11-17 18:37:35 -0700

    backTread test POG

--- IdleNeumont/MainWindow.xaml.cs
+++ IdleNeumont/MainWindow.xaml.cs
@@ -12,17 +12,53 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace IdleNeumont
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+   
     public partial class MainWindow : Window
     {
+        bool genStop = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ThreadStart backRef = new ThreadStart(callBackThread);
+          
+
+            Thread backThread = new Thread(backRef);
+            backThread.Start();
+
+            
         }
+
+        public void callBackThread()
+        {
+
+
+            do
+            {
+                try
+                {
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        numText.Text = (int.Parse(numText.Text) + 1).ToString();
+                    });
+                } catch (TaskCanceledException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                Thread.Sleep(50);
+            } while (!genStop);
+            
+        }
+
     }
 }
